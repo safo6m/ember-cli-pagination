30b03f4fd5ae3bec797f7e6f255a120c549954d7
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2016-07-26 14:37:57 +0200

    Add trigger when new content is fetched

--- addon/remote/paged-remote-array.js
+++ addon/remote/paged-remote-array.js
@@ -92,6 +92,7 @@ export default Ember.ArrayProxy.extend(PageMixin, Ember.Evented, ArrayProxyPromi
         perPage: me.getPerPage()});
 
         me.set("loading",false);
+        this.trigger('newContentLoaded');
         resolve(rows);
         return me.set("meta", metaObj.make());
 
